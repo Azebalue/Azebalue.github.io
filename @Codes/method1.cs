8bc7564b571ae670b062226dd74099a030be8d7f
유저이름 <139879153+Azebalue@users.noreply.github.com>
2025-05-16 07:26:12 +0900

    Create method1.cs

---
+++ @Codes/method1.cs
@@ -0,0 +1,36 @@
+﻿//매개변수: Value Type VS Reference Type
+// + string Type
+
+int[] nums = new int[] { 10 };
+string a = "a";
+
+changeValueTypeVar(nums[0]);
+Console.WriteLine("원본: " + nums[0]);
+
+changeReferenceTypeVar(nums);
+Console.WriteLine("원본: " + nums[0]);
+
+changeStringTypeVar(a);
+Console.WriteLine("원본: " + a);
+
+
+Console.ReadKey();
+
+void changeValueTypeVar(int num)
+{
+    ++num;
+    Console.WriteLine("사본: " + num);
+}
+
+void changeReferenceTypeVar(int[] nums)
+{
+    ++nums[0];
+    Console.WriteLine("사본: " + nums[0]);
+
+}
+void changeStringTypeVar(string  a)
+{
+    a = "b";
+    Console.WriteLine("사본: " + a);
+
+}
