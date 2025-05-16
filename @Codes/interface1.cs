cd63dc06693a37e2aacf0548e4be7351ff5c66c0
유저이름 <139879153+Azebalue@users.noreply.github.com>
2025-05-16 17:10:20 +0900

    update

---
+++ @Codes/interface1.cs
@@ -0,0 +1,86 @@
+﻿IAnimal dog = new Dog();
+IFlyable bird = new Bird();
+IRunable cat = new Cat();
+
+dog.printInformation();
+
+bird.Fly();
+cat.Run();
+
+Console.ReadLine();
+
+public interface IAnimal
+{
+    string Name { get; set; }
+
+    void MakeSound();
+
+    //디폴트 구현
+    void printInformation()
+    {
+        Console.WriteLine($"안녕, 나는 {Name}야");
+    }
+}
+
+public interface IFlyable
+{
+    void Fly();
+}
+
+public interface IRunable
+{
+    void Run();
+
+}
+
+class Bird : IAnimal, IFlyable
+{
+    public string Name { get; set; } = "앵돌이";
+    public void MakeSound()
+    {
+        Console.Write("짹짹");
+    }
+
+    public void Fly()
+    {
+        Console.Write("새가 난다");
+    }
+
+
+
+}
+
+class Dog : IAnimal, IRunable
+{
+    public string Name { get; set; } = "진돌이";
+    public void MakeSound()
+    {
+        Console.Write("멍멍");
+    }
+
+    void IRunable.Run()
+    {
+        Console.Write("강아지가 달림");
+
+    }
+}
+
+
+
+class Cat : IAnimal, IRunable
+{
+    public string Name { get; set; } = "나비";
+
+
+    public void MakeSound()
+    {
+        Console.Write("야옹");
+
+    }
+
+    void IRunable.Run()
+    {
+        Console.Write("고양이가 달림");
+
+    }
+}
--- @Codes/method1.cs
+++ @Codes/method1.cs
@@ -1,18 +1,40 @@
-﻿//매개변수: Value Type VS Reference Type
-// + string Type
+﻿/*매개변수
+ * Value Type VS Reference Type
+ * string Type
+ * ref 키워드
+ * out 키워드
+ * */
 
-int[] nums = new int[] { 10 };
+int[] nums = new int[] { 1 };
 string a = "a";
+string text = "1";
 
 changeValueTypeVar(nums[0]);
-Console.WriteLine("원본: " + nums[0]);
+Console.WriteLine($"원본: {nums[0]}\n");
 
 changeReferenceTypeVar(nums);
-Console.WriteLine("원본: " + nums[0]);
+Console.WriteLine($"원본: {nums[0]}\n");
 
 changeStringTypeVar(a);
-Console.WriteLine("원본: " + a);
+Console.WriteLine($"원본: {a}\n");
 
+usingRef(ref a);
+Console.WriteLine($"원본: {a}\n");
+
+#region ref 키워드
+bool answer = int.TryParse(text, out int result);
+if (answer == true)
+    Console.WriteLine($"성공 결과: {result}");
+else
+    Console.WriteLine($"실패 변환");
+
+answer = int.TryParse(a, out result);
+if (answer == true)
+    Console.WriteLine($"성공 결과: {result}");
+else
+    Console.WriteLine($"변환 실패 ");
+
+#endregion
 
 Console.ReadKey();
 
@@ -28,9 +50,17 @@ void changeReferenceTypeVar(int[] nums)
     Console.WriteLine("사본: " + nums[0]);
 
 }
-void changeStringTypeVar(string  a)
+void changeStringTypeVar(string a)
 {
     a = "b";
     Console.WriteLine("사본: " + a);
 
 }
+
+void usingRef(ref string a)
+{
+    a = "b";
+    Console.WriteLine("사본: " + a);
+
+}
+
