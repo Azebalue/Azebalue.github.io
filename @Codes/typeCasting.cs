74387ea50e6e2c386114154a6cc024bb74916bc7
유저이름 <139879153+Azebalue@users.noreply.github.com>
2025-05-16 17:41:24 +0900

    d

---
+++ @Codes/typeCasting.cs
@@ -0,0 +1,12 @@
+﻿
+int int1 = 100;
+double double1 = int1; //암시적 변환
+Console.WriteLine($"int1:{int1}, double1: {double1}");
+
+double double2 = 123.456; 
+int int2 = (int) double2; //명시적 변환
+Console.WriteLine($"int2:{int2}, double2: {double2}");
+
+
+Console.ReadKey();
+
---
+++ @Codes/typeCasting2.cs
@@ -0,0 +1,36 @@
+﻿
+object stringO = "Hello";
+object intO = 123;
+object floatO = 12.3;
+
+//Unboxing
+Console.WriteLine("문자열 길이"+ ((string)stringO).Length);
+
+//as
+string? str = stringO as string;
+if(str != null)
+    Console.WriteLine(str);
+
+str = intO as string;
+if (str != null)
+    Console.WriteLine(str);
+
+int? intN = stringO as int?;
+if (intN != null)
+    Console.WriteLine(intN);
+
+//ConvertType
+int convertedInt = Convert.ToInt32(floatO);
+Console.WriteLine($"ConvertType : {floatO} => {convertedInt}");
+
+//is 연산자
+if(intO is int)
+{
+    Console.WriteLine($"intO타입: int {(int)intO}");
+}
+else if(intO is string)
+    Console.WriteLine("intO타입: string");
+
+
+Console.ReadKey();
+
